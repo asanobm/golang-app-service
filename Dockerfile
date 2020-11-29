f501cebc97c245fc341ca2b473007058b51c2e8a
Asanobm <kim_bumin@fourglobe.co.jp>
2020-11-30 02:49:07 +0900

    create: app

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# BUILDER
+FROM golang:1.15-alpine AS builder
+WORKDIR /go/src/app
+COPY . .
+RUN go get -d -v ./...
+RUN go build -o main​
+# RUNNING
+FROM debian:buster
+RUN mkdir /app
+COPY --from=builder /go/src/app/main /app/main
+WORKDIR /app/
+CMD ["/app/main"]
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/gin-gonic/gin"
+)
+
+var db *sql.DB
+var server = os.Getenv("DATABASE_SERVER")
+var port = 1433
+var user = os.Getenv("DATABASE_USER")
+var password = os.Getenv("DATABASE_PASSWORD")
+var database = "app-service"
+
+// Conn ...
+func Conn() *sql.DB {
+	// Build connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
+	var err error
+	// Create connection pool
+	db, err = sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal("Error creating connection pool: ", err.Error())
+	}
+	ctx := context.Background()
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Printf("Connected!")
+
+	return db
+}
+
+func main() {
+	Conn()
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
